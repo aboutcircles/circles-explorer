e4461b501087a0d434e4852c07e26abf85d18647
verkhohliad <ihor.verkhohliad@gmail.com>
2024-11-05 09:52:09 +0000

    chore: remove go to avatar page conditional link

--- src/shared/Search.tsx
+++ src/shared/Search.tsx
@@ -1,7 +1,4 @@
-import { Button } from '@nextui-org/react'
 import { useCallback } from 'react'
-import { isAddress, isHash } from 'viem'
-import { useNavigate } from 'react-router-dom'
 
 import { SearchBox } from 'components/SearchBox'
 import { useSearchStore } from 'stores/useSearchStore'
@@ -9,7 +6,6 @@ import { useSearchStore } from 'stores/useSearchStore'
 export function Search() {
 	const updateSearch = useSearchStore.use.updateSearch()
 	const search = useSearchStore.use.search() ?? ''
-	const navigate = useNavigate()
 
 	const handleSubmit = useCallback(
 		(newSearch: string) => {
@@ -18,11 +14,6 @@ export function Search() {
 		[updateSearch]
 	)
 
-	const handleNavigateToAvatar = useCallback(() => {
-		updateSearch('')
-		navigate(`/avatar/${search}`)
-	}, [updateSearch, navigate, search])
-
 	return (
 		<div className='m-4 flex justify-center'>
 			<SearchBox
@@ -30,22 +21,6 @@ export function Search() {
 				handleSubmit={handleSubmit}
 				outerSearch={search}
 			/>
-
-			{isAddress(search) && (
-				<Button
-					onPress={handleNavigateToAvatar}
-					isIconOnly
-					className='ml-2'
-					color='primary'
-					isDisabled={!isAddress(search) && !isHash(search)}
-				>
-					<img
-						src='/icons/arrow-right.svg'
-						alt='GoToAvatar'
-						className='fg-white h-5 w-5'
-					/>
-				</Button>
-			)}
 		</div>
 	)
 }
