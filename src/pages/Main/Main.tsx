c59cb51071418a10226dafa7ce153b3d7af42ef6
verkhohliad <ihor.verkhohliad@gmail.com>
2025-02-18 15:58:52 +0000

    wip

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,31 +1,71 @@
-import { useMemo } from 'react'
 import type { ReactElement } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useEffect, useMemo } from 'react'
+import { useLocation, useSearchParams } from 'react-router-dom'
 import type { Address } from 'viem'
 import { isAddress } from 'viem'
 
+import type { CirclesEventType } from '@circles-sdk/data'
+import { EVENTS } from 'constants/events'
 import { EventsTable } from 'shared/EventsTable'
 import { Filter } from 'shared/Filter'
-import { useSearchSync } from 'hooks/useSearchSync'
-import { Stats } from './Stats'
+import { useFilterStore } from 'stores/useFilterStore'
 import { AvatarSection } from './AvatarSection'
+import { Stats } from './Stats'
 
 export default function Main(): ReactElement {
-	useSearchSync()
-
 	const location = useLocation()
+	const [searchParameters] = useSearchParams()
+	const updateEventTypes = useFilterStore.use.updateEventTypes()
+	const updateSearch = useFilterStore.use.updateSearch()
+	const eventTypes = useFilterStore.use.eventTypes()
+	const syncWithUrl = useFilterStore.use.syncWithUrl()
+
+	// Sync with URL parameters on mount
+	useEffect(() => {
+		syncWithUrl({
+			search: searchParameters.get('search') ?? '',
+			filter: searchParameters.get('filter') ?? ''
+		})
+	}, [searchParameters]) // eslint-disable-line react-hooks/exhaustive-deps
+
+	// // Sync from URL on load
+	// useEffect(() => {
+	// 	const searchParameters = new URLSearchParams(location.search)
+	// 	const searchQuery = searchParameters.get('search')
+	// 	const filterParameter = searchParameters.get('filter')
+	//
+	// 	if (searchQuery) {
+	// 		updateSearch(searchQuery)
+	// 	}
+	//
+	// 	if (filterParameter) {
+	// 		const selectedEvents = filterParameter.split(',') as CirclesEventType[]
+	// 		// First, clear all events except the ones we want to keep
+	// 		for (const event of EVENTS) {
+	// 			if (eventTypes.has(event) && !selectedEvents.includes(event)) {
+	// 				updateEventTypes(event)
+	// 			}
+	// 		}
+	// 		// Then add any missing selected events
+	// 		for (const event of selectedEvents) {
+	// 			if (!eventTypes.has(event)) {
+	// 				updateEventTypes(event)
+	// 			}
+	// 		}
+	// 	}
+	// }, [location.search, eventTypes]) // eslint-disable-line react-hooks/exhaustive-deps
 
 	// it means avatar
 	const { isSearchAddress, search } = useMemo(() => {
-		const searchParameters = new URLSearchParams(location.search)
-
 		const searchParameter = searchParameters.get('search') ?? ''
+
+		console.log({ searchParameter })
 
 		return {
 			isSearchAddress: isAddress(searchParameter),
 			search: searchParameter
 		}
-	}, [location.search])
+	}, [searchParameters])
 
 	return (
 		<div className='flex flex-col'>
