3b25a60ba17232c7240f5b3e983ec356cd21d18a
verkhohliad <ihor.verkhohliad@gmail.com>
2025-02-20 14:23:52 +0000

    CRC: 717 - remove on-hover total stats details for stats cards, show details in the card instead

--- src/components/StatCard.tsx
+++ src/components/StatCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader, CardBody, Spinner, Tooltip } from '@nextui-org/react'
+import { Card, CardHeader, CardBody, Spinner } from '@nextui-org/react'
 
 import { prettifyNumber } from 'utils/number'
 
@@ -11,8 +11,6 @@ interface StatCardProperties {
 	isMobile?: boolean
 }
 
-// const PERCENTAGE = 100
-
 export function StatCard({
 	label,
 	value,
@@ -21,45 +19,38 @@ export function StatCard({
 	progressValue2 = 0,
 	isMobile = false
 }: StatCardProperties) {
-	return (
-		<Tooltip
-			content={`${prettifyNumber(progressValue2)} V2 / ${prettifyNumber(progressValue1)} V1`}
-		>
-			{isMobile ? (
-				<div>
-					<p className='text-sm font-medium text-gray-500'>{label}</p>
-					{isLoading ? (
-						<Spinner />
-					) : (
-						<p className='text-2xl font-semibold text-gray-900'>
-							{prettifyNumber(value)}
-						</p>
-					)}
-				</div>
+	return isMobile ? (
+		<div>
+			<p className='text-sm font-medium text-gray-500'>{label}</p>
+			{isLoading ? (
+				<Spinner />
 			) : (
-				<Card className='m-2 mt-8 w-[165px] border-1 sm:w-[200px] md:w-[305px]'>
-					<CardHeader className='flex pb-1 pl-3 pt-3'>
-						<div className='text-sm text-grayText'>{label}</div>
-					</CardHeader>
-					<CardBody className='pb-3 pl-3 pt-1'>
-						{isLoading ? (
-							<Spinner />
-						) : (
-							<p className='flex text-3xl'>{prettifyNumber(value)}</p>
-						)}
-					</CardBody>
-				</Card>
+				<p className='text-2xl font-semibold text-gray-900'>
+					{prettifyNumber(value)}
+				</p>
 			)}
-		</Tooltip>
+		</div>
+	) : (
+		<Card className='m-2 mt-8 w-[165px] border-1 sm:w-[200px] md:w-[305px]'>
+			<CardHeader className='flex pb-1 pl-3 pt-3'>
+				<div className='text-sm text-grayText'>{label}</div>
+			</CardHeader>
+			<CardBody className='pb-3 pl-3 pt-1'>
+				{isLoading ? (
+					<Spinner />
+				) : (
+					<>
+						<p className='flex text-3xl'>{prettifyNumber(value)}</p>
+						<span className='text-xs'>
+							{prettifyNumber(progressValue2)} V2 /
+							{` ${prettifyNumber(progressValue1)}`} V1
+						</span>
+					</>
+				)}
+			</CardBody>
+		</Card>
 	)
 }
-
-// <Progress
-// 	size='sm'
-// 	color='primary'
-// 	aria-label='Loading V2'
-// 	value={(progressValue2 / progressValue1) * PERCENTAGE}
-// />
 
 StatCard.defaultProps = {
 	isMobile: false,
